ac899f99ff419f83a841f75a7b1b5f2d7e9c49a5
wangzijian <1219116968@qq.com>
2023-07-21 00:03:09 +0800

    feature: 集成Log4j2 做日志框架

--- demo-api/src/main/java/com/example/api/Main.java
+++ demo-api/src/main/java/com/example/api/Main.java
@@ -2,6 +2,7 @@ package com.example.api;
 
 
 import com.example.tools.service.EnvService;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -9,21 +10,28 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import springfox.documentation.oas.annotations.EnableOpenApi;
+import org.apache.logging.log4j.LogManager;
 
 @ComponentScan("com.example")
 @SpringBootApplication
 @EnableOpenApi
 public class Main implements ApplicationRunner {
 
+
+    private  Logger logger = LogManager.getLogger(Main.class);
     @Autowired
     private EnvService envService;
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+
     }
 
     @Override
     public void run(ApplicationArguments args) {
         envService.printSwaggerAddress();
+        logger.info("Hello World!!info ");
+        logger.warn("Hello World!! ww");
+        logger.error("Hello World!!error");
     }
 }
